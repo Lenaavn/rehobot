b7685dee733351e31030ab9b45335e30295755a2
lenaavn <leandro1531@outlook.es>
2025-03-14 13:02:37 +0100

    first commit

---
+++ src/main/java/com/reho/RehobotApplication.java
@@ -0,0 +1,13 @@
+package com.reho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reho/RehobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RehobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
